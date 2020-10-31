d0ff463db4be296f16136892cebd0a3d53a674ec
German Baena <germanb66@gmail.com>
2020-10-30 19:31:53 -0500

    Codigo de respaldo del primer desarrollo del Sitio Web de ICVVP

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+
+addEventListener('DOMContentLoaded', () => {
+
+    // Hamburger button.
+    const   burgerButton = document.querySelector('.burguer-button'),
+            burgerInner = document.querySelector('.burguer-inner'),
+            navMain = document.getElementById('main-nav')
+    
+    burgerButton.addEventListener('click', () => {
+        burgerInner.classList.toggle('cruz')
+        navMain.classList.toggle('main-nav-move')
+    })
+
+
+    // Change menu icons.
+    const   wrapperIcons = document.getElementById('wrapper-icons'),
+            mobileIcons = document.createElement('div'),
+            desktopIcons = document.createElement('div'),
+            mediaQuery = matchMedia('(min-width: 960px)')
+
+    mobileIcons.classList.add('mobile-icons')
+    mobileIcons.id = 'mobile-icons'
+
+    desktopIcons.classList.add('desktop-icons')
+    desktopIcons.id = 'desktop-icons'
+
+
+    const changeIcons = () => {
+    
+
+        if (mediaQuery.matches) {
+            mobileIcons.remove()
+            wrapperIcons.appendChild(desktopIcons)
+            desktopIcons.innerHTML =    `<a href="#" class="checkin"><img src="img/icons/checkin-desk.svg" alt="Icono de registro" class="checkin__icon"></a>
+                                        <a href="#" class="contact"><img src="img/icons/contact-desk.svg" alt="Icono de contacto" class="contact__icon"></a>`
+        } else {
+            desktopIcons.remove()
+            wrapperIcons.appendChild(mobileIcons)
+            mobileIcons.innerHTML =    `<a href="#" class="checkin"><img src="img/icons/checkin-mobl.svg" alt="Icono de registro" class="checkin__icon"></a>
+                                        <a href="#" class="contact"><img src="img/icons/contact-mobl.svg" alt="Icono de contacto" class="contact__icon"></a>`
+        }
+    }
+
+    changeIcons()
+
+    mediaQuery.addListener(changeIcons)
+    
+})
+
+
+
+
+
+
